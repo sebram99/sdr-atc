593b8053db8a8ee5f6631437e49a2013857b1953
sebastian <sebram8@gmail.com>
2024-09-26 23:16:42 -0400

    transmit sine waveform

--- SIFWaveform.py
+++ SIFWaveform.py
@@ -9,6 +9,19 @@ class SIFWaveform:
         pass
 
     def make_sif_waveform(self, p1_pulse : dict, p1_p2_spacing, p2_pulse : dict, p1_p3_spacing, p3_pulse : dict):
+        """
+        Generates a SIF waveform by concatenating three pulses with specified spacings.
+
+        Parameters:
+        p1_pulse (dict): Dictionary containing 'Width' and 'Amplitude' for the first pulse.
+        p1_p2_spacing (float): Time spacing between the end of the first pulse and the start of the second pulse.
+        p2_pulse (dict): Dictionary containing 'Width' and 'Amplitude' for the second pulse.
+        p1_p3_spacing (float): Time spacing between the end of the first pulse and the start of the third pulse.
+        p3_pulse (dict): Dictionary containing 'Width' and 'Amplitude' for the third pulse.
+
+        Returns:
+        numpy.ndarray: Concatenated waveform consisting of the three pulses and their respective spacings.
+        """
         waveform = waveformgen.WaveformGenerator(self.sample_rate)
         p1 = waveform.pulse(p1_pulse['Width'], p1_pulse['Amplitude'])
         p1_p2_space = self.sample_rate*(p1_p2_spacing - p1_pulse['Width'])
@@ -21,6 +34,9 @@ class SIFWaveform:
         p3 = waveform.pulse(p3_pulse['Width'], p3_pulse['Amplitude'])
 
         return np.concatenate((p1, p1_p2_space, p2, p1_p3_space, p3))
+
+    def make_mode_s_waveform(self, p1_pulse : dict, p1_p2_spacing, p2_pulse : dict, p2_p6_spacing, p6_pulse : dict):
+        pass
 
 waveform = SIFWaveform()
 p1_pulse = {'Width' : 800e-9, 'Amplitude' : 0}
